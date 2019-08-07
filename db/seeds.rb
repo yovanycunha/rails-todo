ba8a9c9505db9281db34cc132a37a1b34159eb41
Yovany Cunha <yovany33@gmail.com>
2019-08-07 09:27:12 -0300

    rails back

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Task.create(title: 'Ler os artigo do OneBitCode sobre React', done: false)
+Task.create(title: 'Participar da Semana Super Full Stack (29/04 - 05/05)', done: false)
+Task.create(title: 'Se inscrever na newsletter para receber vagas', done: false)
+Task.create(title: 'Curtir a página do OneBitCode no Facebook', done: false)
+Task.create(title: 'Se inscrever no canal do Youtube do OneBitCode', done: false)
+Task.create(title: 'Finalizar esse Artigo para melhorar minhas habilidades', done: false)
+Task.create(title: 'Estudar Ruby On Rails', done: true)
+Task.create(title: 'Estudar React', done: true)
